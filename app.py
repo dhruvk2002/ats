a254c08f3828d12c332e8e5e518636fe01b38968
dhruvk2002 <dhruvsudhanshu@gmail.com>
2024-01-19 12:33:40 +0530

    Updated title icon

--- app.py
+++ app.py
@@ -40,8 +40,8 @@ def input_pdf_setup(uploaded_file):
 
 ## Streamlit App
 
-st.set_page_config(page_title="ATS Resume EXpert")
-st.header("ATS Tracking System")
+st.set_page_config(page_title="Test your Resume", page_icon="icon.png", layout="centered")
+st.header("ATS Resume Tracking System")
 input_text=st.text_area("Job Description: ",key="input")
 uploaded_file=st.file_uploader("Upload your resume(PDF)...",type=["pdf"])
 
